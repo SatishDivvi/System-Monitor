69d521b72960d729c0f50c94e458933b9f09081c
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-28 21:58:43 -0700

    Add comments

--- process.cpp
+++ process.cpp
@@ -19,10 +19,16 @@ class Process {
         Process(){};
         // Setter for assigining values to class attributes.
         void setProcessAttributes(int pid, string user, string cmd, double cpu_utilization, string memory_utilization, string time);
+        // Getter for PID Attribute
         int getPID() const;
+        // Getter for user attribute
         string getUser() const;
+        // Getter for cmd attribute
         string getCMD() const;
+        // Getter for Cpu_utilization attribute
         double getCPUUsage() const;
+        // Getter for memory_utilization attribute
         string getMemoryUsage() const;
+        // Getter for time attribute
         string getTime() const;
 };
