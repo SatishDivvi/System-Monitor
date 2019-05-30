f7d1322504bfca19e9163c1f18800522a0c5c2cf
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-29 18:19:50 -0700

    Declare and Define ProcessParser Class

--- ProcessParser.h
+++ ProcessParser.h
@@ -18,3 +18,21 @@
 #include <unistd.h>
 #include <constants.h>
 
+class ProcessParser {
+    public:
+        static std::string get_cmd(std::string pid);
+        static std::vector<std::string> get_pid_list();
+        static std::string get_vm_size(std::string pid);
+        static std::string get_cpu_percent(std::string pid);
+        static long int get_sys_up_time();
+        static std::string get_proc_up_time(std::string pid);
+        static std::string get_proc_user(std::string pid);
+        static std::vector<std::string> get_sys_cpu_percent(std::string coreNumber = "");
+        static float get_sys_ram_percent();
+        static std::string get_sys_kernel_version();
+        static int get_total_threads();
+        static int get_total_number_of_processes();
+        static int get_number_of_running_processes();
+        static string get_os_name();
+        static std::string print_cpu_stats(std::vector<std::string> values1, std::vector<std::string>values2);
+};
