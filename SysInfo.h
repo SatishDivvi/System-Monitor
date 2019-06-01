b5f8683ca4f8164741ca73a3baee2e151009cf1d
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-01 15:40:35 -0700

    Declare private attributes

--- SysInfo.h
+++ SysInfo.h
@@ -6,5 +6,18 @@
 using namespace std;
 
 class SysInfo {
-
+    private:
+        vector<string> last_cpu_stats;
+        vector<string> current_cpu_stats;
+        vector<string> core_stats;
+        vector<vector<string>> last_cpu_cores_stats;
+        vector<vector<string>> current_cpu_cores_stats;
+        string cpu_stats;
+        float mem_percent;
+        string os_name;
+        string kernel_ver;
+        long up_time;
+        int total_proc;
+        int running_proc;
+        int threads;
 };
