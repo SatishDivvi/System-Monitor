35f1b2de7d850507ef3b2e2f76adc2f381a68394
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-01 15:45:14 -0700

    Add setter and getters member functions

--- SysInfo.h
+++ SysInfo.h
@@ -20,4 +20,19 @@ class SysInfo {
         int total_proc;
         int running_proc;
         int threads;
+    
+    public:
+        void set_attributes();
+        void set_last_cpu_measures();
+        string get_mem_percent() const;
+        long get_up_time() const;
+        string get_threads() const;
+        string get_total_proc() const;
+        string get_running_proc() const;
+        string get_kernel_version() const;
+        string get_os_name() const;
+        string get_cpu_percent() const;
+        void get_other_cores(int _size);
+        void set_cpu_cores_stats();
+        vector<string> get_cores_stats() const;
 };
