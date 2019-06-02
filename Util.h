d99d919f7378c80b65dc2f2ebe2ebeda70b68f83
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-01 22:55:47 -0700

    Change parameter name to input_seconds

--- Util.h
+++ Util.h
@@ -7,15 +7,15 @@ using namespace std;
 
 class Util {
     public:
-        static string convertToTime(long int seconds);
+        static string convertToTime(long int input_seconds);
         static string getProgressBar(string percent);
         static ifstream getStream(string path);
 };
 
-string Util::convertToTime(long int seconds) {
-    long minutes = seconds / 60;
+string Util::convertToTime(long int input_seconds) {
+    long minutes = input_seconds / 60;
     long hours = minutes / 60;
-    long seconds = int(seconds%60);
+    long seconds = int(input_seconds%60);
     minutes = int(minutes%60);
     string result = to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
     return result;
