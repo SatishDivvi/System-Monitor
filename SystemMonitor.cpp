7fbda5d37b8a2feb0f00f31b389eb66a0969e598
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-06-01 22:28:08 -0700

    Declare headers

---
+++ SystemMonitor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <ncurses.h>
+#include <time.h>
+#include <sstream>
+#include <iomanip>
+#include "util.h"
+#include "SysInfo.h"
+#include "ProcessContainer.h"
+
+using namespace std;
